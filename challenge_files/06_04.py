937fa6d77e74440251c26dbacee7e4d50dd2c391
Jerome H <ralph.jerome.hernandez@outlook.com>
2024-12-04 20:11:58 +1100

    challenge: Triangle number

---
+++ challenge_files/06_04.py
@@ -0,0 +1,12 @@
+def triangle(num):
+    if num == 1:
+        return num
+    return num + triangle(num - 1)
+
+
+def square(num):
+    return triangle(num) + triangle(num - 1)
+
+
+print(square(5))
+print(square(6))
